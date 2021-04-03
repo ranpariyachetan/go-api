a6607ad43bb15a7bff617eed8e9b8eeb962c9480
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2021-04-03 15:27:15 +0800

    First version of articles API

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to home page!!!")
+	fmt.Println("Endpoint hit: Homepage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":8082", nil))
+}
+
+func main() {
+	Articles = []Article{
+		Article{Title: "Hello1", Desc: "Hello Article 1", Content: "Hello Article 1 Content"},
+		Article{Title: "Hello2", Desc: "Hello Article 2", Content: "Hello Article 2 Content"},
+	}
+	handleRequests()
+	//	fmt.Println("Hello world")
+}
